796f7313a97121a8054ab54d745fac26373cf061
Jun <me@noraesae.net>
2016-12-03 19:17:01 +0900

    Add test cases for Line.Messaging.Webhook.Types

--- test/Line/Messaging/Webhook/TypesSpec.hs
+++ test/Line/Messaging/Webhook/TypesSpec.hs
@@ -9,6 +9,7 @@ import Data.Maybe (isJust)
 import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Line.Messaging.Webhook.Types
+import Line.Messaging.Types (Text(..), Sticker(..), Location(..))
 import qualified Data.ByteString.Lazy as BL
 
 fromJSONSpec :: (FromJSON a, Eq a, Show a)
@@ -25,18 +26,109 @@ datetime = posixSecondsToUTCTime . (/ 1000) . fromInteger
 
 spec :: Spec
 spec = do
-  describe "parse body" $ fromJSONSpec
+  describe "body" $ fromJSONSpec
     [ ( goodBody, Just $ Body [] )
     , ( badBody, Nothing)
     ]
 
-  describe "parse beacon event" $ fromJSONSpec
-    [ ( goodBeacon, Just $ Body $ [
-          BeaconEvent ( User "U206d25c2ea6bd87c17655609a1c37cb8"
+  describe "event source" $ fromJSONSpec
+    [ ( goodUser, Just $ User "123" )
+    , ( goodGroup, Just $ Group "456" )
+    , ( goodRoom, Just $ Room "789" )
+    , ( badSource, Nothing )
+    ]
+
+  let replyE event a = Just $ Body $ [
+        event ( User "U206d25c2ea6bd87c17655609a1c37cb8"
+              , datetime 1462629479859
+              , "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA"
+              , a
+              )
+        ]
+  let nonreplyE event a = Just $ Body $ [
+        event ( User "U206d25c2ea6bd87c17655609a1c37cb8"
+              , datetime 1462629479859
+              , ()
+              , a
+              )
+        ]
+
+  describe "text message event" $ fromJSONSpec
+    [ ( goodTextMessage, replyE MessageEvent (TextEM "325708" $ Text "Hello, world") )
+    , ( badTextMessage, Nothing )
+    ]
+
+  describe "image message event" $ fromJSONSpec
+    [ ( goodImageMessage, replyE MessageEvent (ImageEM "325708") )
+    , ( badImageMessage, Nothing )
+    ]
+
+  describe "video message event" $ fromJSONSpec
+    [ ( goodVideoMessage, replyE MessageEvent (VideoEM "325708") )
+    , ( badVideoMessage, Nothing )
+    ]
+
+  describe "audio message event" $ fromJSONSpec
+    [ ( goodAudioMessage, replyE MessageEvent (AudioEM "325708") )
+    , ( badAudioMessage, Nothing )
+    ]
+
+  describe "location message event" $ fromJSONSpec
+    [ ( goodLocationMessage, replyE MessageEvent (LocationEM "325708" $
+                                               Location
+                                                "my location"
+                                                "some address"
+                                                35.65910807942215
+                                                139.70372892916203) )
+    , ( badLocationMessage, Nothing )
+    ]
+
+  describe "stcker message event" $ fromJSONSpec
+    [ ( goodStickerMessage, replyE MessageEvent (StickerEM "325708" $
+                                                 Sticker
+                                                  "1"
+                                                  "1") )
+    , ( badStickerMessage, Nothing )
+    ]
+
+  describe "follow event" $ fromJSONSpec
+    [ ( goodFollow, replyE FollowEvent () )
+    , ( badFollow, Nothing )
+    ]
+
+  describe "unfollow event" $ fromJSONSpec
+    [ ( goodUnfollow, nonreplyE UnfollowEvent () )
+    , ( badUnfollow, Nothing )
+    ]
+
+  describe "join event" $ fromJSONSpec
+    [ ( goodJoin, Just $ Body $ [
+        JoinEvent ( Group "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+                  , datetime 1462629479859
+                  , "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA"
+                  , ()
+                  )
+        ] )
+    , ( badJoin, Nothing )
+    ]
+
+  describe "leave event" $ fromJSONSpec
+    [ ( goodLeave, Just $ Body $ [
+          LeaveEvent ( Group "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
                      , datetime 1462629479859
-                     , "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA"
-                     , BeaconEnter "d41d8cd98f"
+                     , ()
+                     , ()
                      )
-          ])
-    , ( badBeacon, Nothing)
+          ] )
+    , ( badLeave, Nothing )
+    ]
+
+  describe "postback event" $ fromJSONSpec
+    [ ( goodPostback, replyE PostbackEvent "action=buyItem&itemId=123123&color=red" )
+    , ( badPostback, Nothing )
+    ]
+
+  describe "beacon event" $ fromJSONSpec
+    [ ( goodBeacon, replyE BeaconEvent (BeaconEnter "d41d8cd98f") )
+    , ( badBeacon, Nothing )
     ]
--- test/Line/Messaging/Webhook/TypesSpecHelper.hs
+++ test/Line/Messaging/Webhook/TypesSpecHelper.hs
@@ -15,6 +15,421 @@ badBody = [r|
 { "event": [] }
 |]
 
+goodUser :: BL.ByteString
+goodUser = [r|
+{ "type": "user", "userId": "123" }
+|]
+
+goodGroup :: BL.ByteString
+goodGroup = [r|
+{ "type": "group", "groupId": "456" }
+|]
+
+goodRoom :: BL.ByteString
+goodRoom = [r|
+{ "type": "room", "roomId": "789" }
+|]
+
+badSource :: BL.ByteString
+badSource = [r|
+{ "type": "bad", "userId": "123 }
+|]
+
+goodTextMessage :: BL.ByteString
+goodTextMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "text",
+    "text": "Hello, world"
+  }
+}
+] }
+|]
+
+badTextMessage :: BL.ByteString
+badTextMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "text'",
+    "text": "Hello, world"
+  }
+}
+] }
+|]
+
+goodImageMessage :: BL.ByteString
+goodImageMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "image"
+  }
+}
+
+] }
+|]
+
+badImageMessage :: BL.ByteString
+badImageMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "image'"
+  }
+}
+] }
+|]
+
+goodVideoMessage :: BL.ByteString
+goodVideoMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "video"
+  }
+}
+] }
+|]
+
+badVideoMessage :: BL.ByteString
+badVideoMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "video'"
+  }
+}
+] }
+|]
+
+goodAudioMessage :: BL.ByteString
+goodAudioMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "audio"
+  }
+}
+] }
+|]
+
+badAudioMessage :: BL.ByteString
+badAudioMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "audio'"
+  }
+}
+] }
+|]
+
+goodLocationMessage :: BL.ByteString
+goodLocationMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "location",
+    "title": "my location",
+    "address": "some address",
+    "latitude": 35.65910807942215,
+    "longitude": 139.70372892916203
+  }
+}
+] }
+|]
+
+badLocationMessage :: BL.ByteString
+badLocationMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id'": "325708",
+    "type": "location",
+    "title": "my location",
+    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+    "latitude": 35.65910807942215,
+    "longitude": 139.70372892916203
+  }
+}
+] }
+|]
+
+goodStickerMessage :: BL.ByteString
+goodStickerMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "sticker",
+    "packageId": "1",
+    "stickerId": "1"
+  }
+}
+] }
+|]
+
+badStickerMessage :: BL.ByteString
+badStickerMessage = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message'": {
+    "id": "325708",
+    "type": "sticker",
+    "packageId": "1",
+    "stickerId": "1"
+  }
+}
+] }
+|]
+
+goodFollow :: BL.ByteString
+goodFollow = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "follow",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  }
+}
+] }
+|]
+
+badFollow :: BL.ByteString
+badFollow = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "follow'",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  }
+}
+] }
+|]
+
+goodUnfollow :: BL.ByteString
+goodUnfollow = [r|
+{ "events": [
+{
+  "type": "unfollow",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  }
+}
+] }
+|]
+
+badUnfollow :: BL.ByteString
+badUnfollow = [r|
+{ "events": [
+{
+  "type": "unfollow'",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  }
+}
+] }
+|]
+
+goodJoin :: BL.ByteString
+goodJoin = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "join",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "group",
+    "groupId": "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+}
+] }
+|]
+
+badJoin :: BL.ByteString
+badJoin = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "join'",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "group",
+    "groupId": "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+}
+] }
+|]
+
+goodLeave :: BL.ByteString
+goodLeave = [r|
+{ "events": [
+{
+  "type": "leave",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "group",
+    "groupId": "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+}
+] }
+|]
+
+badLeave :: BL.ByteString
+badLeave = [r|
+{ "events": [
+{
+  "type": "leave'",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "group",
+    "groupId": "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+}
+] }
+|]
+
+goodPostback :: BL.ByteString
+goodPostback = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "postback",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "postback": {
+    "data": "action=buyItem&itemId=123123&color=red"
+  }
+}
+] }
+|]
+
+badPostback :: BL.ByteString
+badPostback = [r|
+{ "events": [
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "postback",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "postback": {
+    "data'": "action=buyItem&itemId=123123&color=red"
+  }
+}
+] }
+|]
+
 goodBeacon :: BL.ByteString
 goodBeacon = [r|
 { "events": [
